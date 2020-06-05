f38127e27739e5e80564bcd5eeb853e48e5d9dbc
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-06-05 23:34:38 +0100

    change config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://livraison-colis-1ef7d.web.app/api/v1/",
-  firebaseConfig: {
-    apiKey: "AIzaSyCzprleyn_DMB5cw9g0nLZxa9FE4iOnX0Y",
-    authDomain: "livraison-colis-1ef7d.firebaseapp.com",
-    databaseURL: "https://livraison-colis-1ef7d.firebaseio.com",
-    projectId: "livraison-colis-1ef7d",
-    storageBucket: "livraison-colis-1ef7d.appspot.com",
-    messagingSenderId: "868728756162",
-    appId: "1:868728756162:web:6b7053562390f2515da85b",
-    measurementId: "G-RE91WYXBBV"
-  }
+  
 };
